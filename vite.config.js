05993dca99591dfa9ae18dd563a1fa62a6d6f04d
Umut Gür <umutgur7@gmail.com>
2026-03-02 12:19:09 +0300

    version update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
+import pkg from "./package.json";
 
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version)
+  },
   clearScreen: false,
   server: {
     port: 5173,
